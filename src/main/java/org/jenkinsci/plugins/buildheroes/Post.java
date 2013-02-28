6d6d39ac2f3a89a833c18a967360806e33c3fc59
Andi Bade <andi@galaxycats.com>
2013-02-28 17:17:32 +0100

    use right callback url

--- src/main/java/org/jenkinsci/plugins/buildheroes/Post.java
+++ src/main/java/org/jenkinsci/plugins/buildheroes/Post.java
@@ -9,33 +9,32 @@ import org.apache.commons.httpclient.methods.PostMethod;
 public class Post {
 
 	private final static Logger log = Logger.getLogger(Post.class.getName());
-	
+
 	public static void sendMessage(String token, String payload){
-        
-//		String url = "http://localhost:1234/api/projects/" + token + "/builds";
-		String url = "https://www.buildheroes.com/marko/uk6gaf6wyub7web9py5swed5";
+
+        String url = "https://www.buildheroes.com/api/projects/" + token + "/builds";
 		String responseBody = "";
-		
+
         try {
             HttpClient client = new HttpClient();
             PostMethod method = new PostMethod(url);
 			method.setRequestHeader("Content-Type", "application/json");
 
             method.addParameter("payload", payload);
-            
+
             log.info("Sending a post message to " + url + " now.");
             int statusCode = client.executeMethod(method);
 
             if (statusCode != -1) {
                 responseBody = method.getResponseBodyAsString();
             }
-            
+
             log.info("Message has been sent, response body is: " + responseBody);
 
         } catch (Exception e) {
             log.severe(e.getMessage() + " STACKTRACE: " + e.getStackTrace().toString());
         }
-        
+
         log.info("Notification is done, buildheroes exits now.");
     }
 }
